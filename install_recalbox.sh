6c4f8df6996436043abcec734e7650419adeaef7
cyperghost <crcerror@users.noreply.github.com>
2019-03-19 21:59:17 +0100

    Update install_recalbox.sh

--- install_recalbox.sh
+++ install_recalbox.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /recalbox
-wget https://raw.githubusercontent.com/crcerror/retroflag-picase/master/other_os/recalbox_install.sh
+touch recalbox.install
+wget -q https://raw.githubusercontent.com/crcerror/retroflag-picase/master/other_os/recalbox_install.sh
 sleep 1
 chmod +x recalbox_install.sh
 ./recalbox_install.sh &
