71b416caebcb78392675566447ed7435089f3ef3
cyperghost <crcerror@users.noreply.github.com>
2019-03-19 22:10:11 +0100

    Create install_batocera.sh

---
+++ install_batocera.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /recalbox
+touch batocera.install
+wget -q https://raw.githubusercontent.com/crcerror/retroflag-picase/master/other_os/recalbox_install.sh
+sleep 1
+chmod +x recalbox_install.sh
+./recalbox_install.sh &
