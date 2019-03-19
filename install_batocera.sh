962927e6326243a43bfab6d797d7ddd6698c0c4d
cyperghost <crcerror@users.noreply.github.com>
2019-03-19 21:29:54 +0100

    Delete install_batocera.sh

--- install_batocera.sh
+++
@@ -1,1 +0,0 @@
-cd ~ && touch batocera.install && wget https://raw.githubusercontent.com/crcerror/retroflag-picase/master/other_os/recalbox_install.sh && bash recalbox_install.sh && rm recalbox_install.sh
