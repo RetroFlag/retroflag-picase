f8f9415703be7a9c1a0f1812438ce64c33e84fa7
cyperghost <crcerror@users.noreply.github.com>
2019-03-20 20:04:37 +0100

    Final!
    Fixed typo!

--- other_os/recalbox_SafeShutdown.py
+++ other_os/recalbox_SafeShutdown.py
@@ -49,7 +49,7 @@ def reset():
 		output = int(subprocess.check_output(['./recalbox_SafeShutdown.sh', '--espid']))
  		output_rc = int(subprocess.check_output(['./recalbox_SafeShutdown.sh', '--emupid']))
  		if output_rc:
-			os.system(scrtipDir + "/recalbox_SafeShutdown.sh --emukill")
+			os.system(scriptDir + "/recalbox_SafeShutdown.sh --emukill")
  		elif output:
 			os.system(scriptDir + "/recalbox_SafeShutdown.sh --restart")
 		else:
