5f77a690ce7c8263e7b713ab1350eb46adfbec00
cyperghost <crcerror@users.noreply.github.com>
2019-09-28 13:33:51 +0200

    Updated for BATOCERA 5.24

--- install_batocera.sh
+++ install_batocera.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Downloading install script .... for BATOCERA"
 sleep 2
-wget -q -O - https://raw.githubusercontent.com/crcerror/retroflag-picase/master/other_os/recalbox_install.sh | bash
+wget -q -O - https://raw.githubusercontent.com/crcerror/retroflag-picase/master/other_os/batocera_install.sh | bash
