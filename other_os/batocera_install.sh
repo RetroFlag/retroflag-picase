ac2aa4b3fd4c3ee4d0b58c688a6b26ca7285aa81
cyperghost <crcerror@users.noreply.github.com>
2019-09-28 13:32:45 +0200

    Create batocera_install.sh

---
+++ other_os/batocera_install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Retroflag Advanced Shutdown Mod by crcerror
+# This is exclusivly for BATOCERA
+# Intended to work for BATOCERA versions 5.23
+#
+# Release Power Button to shutdown your Raspberry
+# Press Reset during ES to reload ES
+# Press Reset in running emulator will kick you back to ES
+
+version=$(grep -o '^[^ ]*' $HOME/data.version)
+git_url="https://raw.githubusercontent.com/crcerror/retroflag-picase/master/other_os/batocera_safeshutdown.py"
+file_dest="/usr/bin/retroflag_SafeShutdown.py"
+
+if [[ ${version//[^[:digit:]]/} -lt 523 ]]; then
+    echo "Error! Please try annother installer"
+    echo "Your current version of Batocera is '$version'"
+    echo "You need at least 5.23 ...."
+    exit
+fi 
+
+echo "Welcome to the Safe Shutdown installer..."
+echo "Batocera '$version' detected..."
+sleep 2
+
+echo
+echo "Downloading script now!"
+wget -q --show-progress "$git_url" -O "$file_dest"
+sleep 2
+
+echo
+echo "Activating UART ... now"
+mount -o remount, rw /boot
+batocera-settings /boot/config.txt activate enable_uart
+
+echo "Activate RETROFLAG in batocera.conf"
+batocera-settings set system.power.switch RETROFLAG
+sleep 2
+
+echo
+echo "Making changes permanent..."
+batocera-save-overlay
+
+echo; echo
+echo "Rebooting in 5 seconds...."
+sleep 5
+shutdown -r now
