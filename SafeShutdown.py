6dff2821fc1464c3e7f4582fd4d3513831ea78f7
shirizaan <shirizaan@gmail.com>
2020-03-09 10:55:19 -0500

    Update to use subprocess instead of os module and corrected process name for EmulationStation.

--- SafeShutdown.py
+++ SafeShutdown.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python3
-from gpiozero import Button, LED
-import os 
-from signal import pause
 
-powerPin = 3 
-resetPin = 2 
-ledPin = 14 
-powerenPin = 4 
+import time
+import subprocess
+from signal import pause
+from gpiozero import Button, LED
+
+powerPin = 3
+resetPin = 2
+ledPin = 14
+powerenPin = 4
 hold = 1
 led = LED(ledPin)
 led.on()
 power = LED(powerenPin)
 power.on()
 
-#functions that handle button events
+# functions that handle button events
 def when_pressed():
-  led.blink(.2,.2)
-  os.system("sudo killall emulationstation && sleep 5s && sudo shutdown -h now")
+    led.blink(0.2, 0.2)
+    subprocess.run("sudo killall emulationstation", shell=True)
+    time.pause(5)
+    subprocess.run("sudo shutdown -h now", shell=True)
+
+
 def when_released():
-  led.on()
-def reboot(): 
-  os.system("sudo killall emulationstation && sleep 5s && sudo reboot")
-  
+    led.on()
+
+
+def reboot():
+    subprocess.run("sudo killall emulationstation", shell=True)
+    time.pause(5)
+    subprocess.run("sudo reboot", shell=True)
+
+
 btn = Button(powerPin, hold_time=hold)
 rebootBtn = Button(resetPin)
-rebootBtn.when_pressed = reboot 
+rebootBtn.when_pressed = reboot
 btn.when_pressed = when_pressed
 btn.when_released = when_released
 pause()
