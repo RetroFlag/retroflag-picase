0d10fab4730f7dfc25ffe7cbb4f380a1af55761e
Devin Flake <flakeman2@hotmail.com>
2021-01-11 22:50:40 -0700

    adding scripts for Ubuntu on Raspberry Pi

---
+++ Ubuntu_SafeShutdown.py
@@ -0,0 +1,71 @@
+import RPi.GPIO as GPIO
+import os
+import time
+from multiprocessing import Process
+
+#initialize pins
+powerPin = 3 #pin 5
+ledPin = 14 #TXD
+resetPin = 2 #pin 13
+powerenPin = 4 #pin 5
+
+#initialize GPIO settings
+def init():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(powerPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(resetPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(ledPin, GPIO.OUT)
+    GPIO.output(ledPin, GPIO.HIGH)
+    GPIO.setup(powerenPin, GPIO.OUT)
+    GPIO.output(powerenPin, GPIO.HIGH)
+
+#waits for user to hold button up to 1 second before issuing poweroff command
+def poweroff():
+    while True:
+        #self.assertEqual(GPIO.input(powerPin), GPIO.LOW)
+        GPIO.wait_for_edge(powerPin, GPIO.FALLING)
+        print("Powering off in 5 seconds")
+        os.system("sleep 5s")
+        os.system("poweroff")
+
+#blinks the LED to signal button being pushed
+def ledBlink():
+    while True:
+        GPIO.output(ledPin, GPIO.HIGH)
+        #self.assertEqual(GPIO.input(powerPin), GPIO.LOW)
+        GPIO.wait_for_edge(powerPin, GPIO.FALLING)
+        start = time.time()
+        while GPIO.input(powerPin) == GPIO.LOW:
+            GPIO.output(ledPin, GPIO.LOW)
+            time.sleep(0.2)
+            GPIO.output(ledPin, GPIO.HIGH)
+            time.sleep(0.2)
+
+#resets the pi
+def reset():
+    while True:
+        #self.assertEqual(GPIO.input(resetPin), GPIO.LOW)
+        GPIO.wait_for_edge(resetPin, GPIO.FALLING)
+        print("Reseting in 5 seconds")
+        os.system("sleep 5s")
+        os.system("shutdown -r now")
+
+
+if __name__ == "__main__":
+    #initialize GPIO settings
+    init()
+    #create a multiprocessing.Process instance for each function to enable parallelism 
+    powerProcess = Process(target = poweroff)
+    powerProcess.start()
+    ledProcess = Process(target = ledBlink)
+    ledProcess.start()
+    resetProcess = Process(target = reset)
+    resetProcess.start()
+
+    powerProcess.join()
+    ledProcess.join()
+    resetProcess.join()
+
+    GPIO.cleanup()
+
