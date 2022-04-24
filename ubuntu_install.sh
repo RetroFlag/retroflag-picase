7095ca7f634a84bde857e6d0cb198ea5e4eef955
Roberto Focosi <rfocosi@gmail.com>
2022-04-24 20:19:21 -0300

    Add more binary checks to ubuntu_install

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
 
+#DEV_ROOT=
+
+CURL_EXEC=$( which curl )
+PYTHON_EXEC=$( which python | which python3 )
+WGET_EXEC=$( which wget )
+
 [ -z $SourcePath ] && SourcePath=https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master
 
 
 #Check if root--------------------------------------
 if [[ $EUID -ne 0 ]]; then
+   echo
    echo "Please execute script as root." 
    exit 1
 fi
-#-----------------------------------------------------------
+#---------------------------------------------------
+
+if [[ -z "$WGET_EXEC" ]]; then
+	echo
+	echo "Requires 'wget'. Please install and try again."
+	exit 1
+fi
+
+if [[ -z "$CURL_EXEC" ]]; then
+	echo
+	echo "Requires 'curl'. Please install and try again."
+	exit 1
+fi
+
+if [[ -z "$PYTHON_EXEC" || ! "$( $PYTHON_EXEC -V )" = "Python 3"* ]]; then
+	echo
+	echo "Requires 'python3'. Please install and try again."
+	exit 1
+fi
+
+if [[ -z "$( python3 -c "import RPi.GPIO as GPIO; print(GPIO.VERSION)" )" ]]; then
+	echo
+	echo "Library 'python3-rpi.gpio' not found. Please install and try again."
+	exit 1
+fi
 
 #RetroFlag pw io ;2:in ;3:in ;4:in ;14:out 1----------------------------------------
-File=/boot/firmware/config.txt
-wget -O  "/boot/firmware/overlays/RetroFlag_pw_io.dtbo" "$SourcePath/RetroFlag_pw_io.dtbo"
+[[ ! -z "$DEV_ROOT" ]] && mkdir -p $DEV_ROOT/boot/firmware/overlays
+File=$DEV_ROOT/boot/firmware/config.txt
+$WGET_EXEC -q -O  "$DEV_ROOT/boot/firmware/overlays/RetroFlag_pw_io.dtbo" "$SourcePath/RetroFlag_pw_io.dtbo"
 if grep -q "RetroFlag_pw_io" "$File";
 	then
 		sed -i '/RetroFlag_pw_io/c dtoverlay=RetroFlag_pw_io.dtbo' $File 
@@ -34,13 +66,14 @@ fi
 #-----------------------------------------------------------
 
 #Download Python script-----------------------------
-mkdir "/opt/RetroFlag"
-script=/opt/RetroFlag/SafeShutdown.py
-wget -O $script "$SourcePath/ubuntu_SafeShutdown.py"
+mkdir -p "$DEV_ROOT/opt/RetroFlag"
+script=$DEV_ROOT/opt/RetroFlag/SafeShutdown.py
+$WGET_EXEC -q -O $script "$SourcePath/ubuntu_SafeShutdown.py"
 
 #Enable Python script to run on start up------------
-service=/lib/systemd/system/safe-shutdown.service
-wget -O $service "$SourcePath/ubuntu_safe-shutdown.service"
+[[ ! -z "$DEV_ROOT" ]] && mkdir -p $DEV_ROOT/lib/systemd/system/
+service=$DEV_ROOT/lib/systemd/system/safe-shutdown.service
+curl -s "$SourcePath/ubuntu_safe-shutdown.service" | awk '{gsub(/PYTHON_EXEC/,"'$PYTHON_EXEC'")}1' > $service
 
 systemctl enable safe-shutdown
 echo "Added service '$service' to system startup."
